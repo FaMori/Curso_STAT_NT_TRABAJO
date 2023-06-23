3e190de9b8a914abc0c2ecaed7547fd73b188e84
FaMori <facur.morini@gmail.com>
2023-06-23 16:25:20 -0300

    Cambios modelo y añadido diagrama app

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(geouy)
 library(shiny)
 library(ggiraph)
+library(networkD3)
 
 #CARGA DATOS
 datos <- read_csv2(here("Datos","aprovechamientos_app.csv"))
@@ -43,72 +44,108 @@ ui <- fluidPage(
                   selected = "Todos")
     ),
     mainPanel(
-      girafeOutput("mapa")
+      tabsetPanel(
+        tabPanel("Uso por cuenca",
+          girafeOutput("mapa"),
+          sankeyNetworkOutput("tabla")),
+        tabPanel("Distribucion de registros",
+                 girafeOutput("registros")))
     )
   )
 )
 
 #SE DEFINE EL SERVER
-server <- function(input, output) {
-
-#VARIABLE REACTIVA DE LOS DATOS QUE SE VAN A UTILIZAR PARA CADA INPUT
-  datos_r <- reactive({
-    if (input$mapa_selector == "Cuencas nivel 1" && input$uso_selector == "Todos") {
-       datos_c1
-    } else if (input$mapa_selector == "Cuencas nivel 2" && input$uso_selector == "Todos") {
-       datos_c2
-    } else if (input$mapa_selector == "Cuencas nivel 1" && input$uso_selector != "Todos") {
-      datos_c1 %>%
-      filter(uso == input$uso_selector)
-    } else if (input$mapa_selector == "Cuencas nivel 2" && input$uso_selector != "Todos") {
-      datos_c2 %>%
-      filter(uso == input$uso_selector)
-    }
+server <- function(input, output, session) {
+  
+  mapa <- reactive({
+    switch(input$mapa_selector,
+           "Cuencas nivel 1" = mapa_c1,
+           "Cuencas nivel 2" = mapa_c2
+    )
+  })
+  
+  datos <- reactive({
+    switch(input$mapa_selector,
+           "Cuencas nivel 1" = datos_c1 %>% 
+             filter(uso %in% ifelse(input$uso_selector == "Todos", unique(uso), input$uso_selector)),
+           "Cuencas nivel 2" = datos_c2 %>% 
+             filter(uso %in% ifelse(input$uso_selector == "Todos", unique(uso), input$uso_selector))
+    )
   })
 
-#DEFINO QUE MAPA SE VA A UTILIZAR EN CADA INPUT  
-  mapa_r <- reactive({
-    if (input$mapa_selector == "Cuencas nivel 1") {
-      mapa_c1
-    } else if (input$mapa_selector == "Cuencas nivel 2") {
-      mapa_c2
-    }
-  })
-
-#SE DEFINE EL MAPA
   output$mapa <- renderGirafe({
 
-#ENCUENTRO EL USO DESTINO PRINCIPAL POR CUENCA
-  reg_cuenca <- datos_r() %>%
-                group_by(codcuenca) %>%
-                summarise(n_reg = sum(n_reg)) %>%
-                select(codcuenca,n_reg)
+    reg_cuenca <- datos() %>%
+      group_by(codcuenca) %>%
+      summarise(n_reg = sum(n_reg)) %>%
+      select(codcuenca,n_reg)
     
-  uso_cuenca <- datos_r() %>%
-                group_by(codcuenca,uso,destino) %>%
-                summarise(v_anual = sum(v_anual)) %>%
-                ungroup() %>%
-                group_by(codcuenca) %>%
-                filter(v_anual == max(v_anual)) %>%
-                select(codcuenca,uso,destino)
-  
-  mapa_fill <- ifelse(input$uso_selector == "Todos","uso","destino")
-  df_mapa <- mapa_r()
-  df_mapa <- left_join(df_mapa,reg_cuenca,by="codcuenca") %>%
-             left_join(uso_cuenca,by="codcuenca",multiple = "all")
-  
-#HAGO EL MAPA
-  mapa <- ggplot() +
+    uso_cuenca <- datos() %>%
+      group_by(codcuenca,uso,destino) %>%
+      summarise(v_anual = sum(v_anual)) %>%
+      group_by(codcuenca) %>%
+      filter(v_anual == max(v_anual)) %>%
+      select(codcuenca,uso,destino)
+    
+    mapa_fill <- ifelse(input$uso_selector == "Todos","uso","destino")
+    
+    df_mapa <- mapa()
+    
+    df_mapa <- left_join(df_mapa,reg_cuenca,by="codcuenca") %>%
+               left_join(uso_cuenca,by="codcuenca",multiple = "all")
+    
+    #HAGO EL MAPA
+    mapa <- ggplot() +
       geom_sf_interactive(data = df_mapa, color = "black",
                           aes(tooltip = paste(nomcuenca, "\nCantidad de registros: ", n_reg,
                                               "\nUso: ", uso,
                                               "\nDestino principal: ", destino),fill=!!sym(mapa_fill),
                               data_id = codcuenca),
-                          hover_css = "fill:blue;", size = 1.5, alpha = 0.8) +  
+                          hover_css = "fill:darkblue;", size = 1.5, alpha = 0.8) +  
       theme_void()
-
-#LO VUELVO INTERACTIVO CON ggiraph    
+    
+    #LO VUELVO INTERACTIVO CON ggiraph    
     girafe(ggobj = mapa)
+  })
+  
+  output$tabla <- renderSankeyNetwork({
+    
+    if (is.null(input$mapa_selected)) {
+      datos_sk <- datos()
+    } else {
+      datos_sk <- datos() %>%
+      filter(codcuenca %in% input$mapa_selected)
+    }
+    
+    datos_sankey <- datos_sk %>%
+                    ungroup() %>%
+                    group_by(uso,destino) %>%
+                    summarise(vol_anual = sum(v_anual)) 
+    
+    links_sankey <- data.frame(
+      source = c(rep("Volumen Anual Disponible",length(datos_sankey$uso)),
+                 datos_sankey$uso), 
+      target = c(datos_sankey$uso,datos_sankey$destino), 
+      value = c(datos_sankey$vol_anual,datos_sankey$vol_anual)  
+    ) 
+    
+    if(input$uso_selector != "Todos"){links_sankey  <- filter(links_sankey,source != "Volumen Anual Disponible")}
+    else{links_sankey  <- filter(links_sankey,target != "Otros")}
+    
+    nodos_sankey <- data.frame(
+      name=c(as.character(links_sankey$source), 
+             as.character(links_sankey$target)) %>% unique()
+    )
+    
+    links_sankey$IDsource <- match(links_sankey$source, nodos_sankey$name)-1 
+    links_sankey$IDtarget <- match(links_sankey$target, nodos_sankey$name)-1
+    
+    sankey_vol <- sankeyNetwork(Links = links_sankey, Nodes = nodos_sankey,
+                                Source = "IDsource", Target = "IDtarget",
+                                Value = "value", NodeID = "name", 
+                                sinksRight=FALSE)
+    
+    sankey_vol
   })
 }
 
